167ca10ebf6a7be26ec27054f07e59e85b64fd93
Daniil Avdeev <daniil.avdeev.2001@yandex.ru>
2023-08-20 17:51:00 +0300

    Initial commit

---
+++ Ex_1/Homework_2_3_1.cpp
@@ -0,0 +1,150 @@
+﻿#include <iostream>
+
+
+class Calculator
+{
+private:
+
+    double num1 = 0;
+    double num2 = 0;
+
+public:
+
+    double add()
+    {
+        return num1 + num2;
+    }
+
+    double multiply()
+    {
+        return num1 * num2;
+    }
+
+    double subtract_1_2()
+    {
+        return num1 - num2;
+    }
+
+    double subtract_2_1()
+    {
+        return num2 - num1;
+    }
+
+    double divide_1_2()
+    {
+        return num1 / num2;
+    }
+
+    double divide_2_1()
+    {
+        return num2 / num1;
+    }
+
+    bool set_num1(double num1)
+    {
+        if (num1 == 0)
+        {
+            return false;
+        }
+        else
+        {
+            this->num1 = num1;
+
+            return true;
+        }
+    }
+
+    bool set_num2(double num2)
+    {
+        if (num2 == 0)
+        {
+            return false;
+        }
+        else
+        {
+            this->num2 = num2;
+
+            return true;
+        }
+    }
+};
+
+
+void set_num1(Calculator* calc)
+{
+    std::cout << "Введите num1: ";
+    int num1 = 0;
+    std::cin >> num1;
+    if (!calc->set_num1(num1))
+    {
+        std::cout << "Неверный ввод!" << std::endl;
+
+        set_num1(calc);
+    }
+}
+
+
+void set_num2(Calculator* calc)
+{
+    std::cout << "Введите num2: ";
+    int num2 = 0;
+    std::cin >> num2;
+    if (!calc->set_num2(num2))
+    {
+        std::cout << "Неверный ввод!" << std::endl;
+
+        set_num2(calc);
+    }
+}
+
+
+void print_results(Calculator* calc)
+{
+    std::cout << "num1 + num2 = " << calc->add() << std::endl;
+    std::cout << "num1 * num2 = " << calc->multiply() << std::endl;
+    std::cout << "num1 - num2 = " << calc->subtract_1_2() << std::endl;
+    std::cout << "num2 - num1 = " << calc->subtract_2_1() << std::endl;
+    std::cout << "num1 / num2 = " << calc->divide_1_2() << std::endl;
+    std::cout << "num2 / num1 = " << calc->divide_2_1() << std::endl;
+}
+
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    system("chcp 1251");
+
+
+    Calculator calc;
+
+    bool endOfProg = false;
+    do {
+        set_num1(&calc);
+        set_num2(&calc);
+        print_results(&calc);
+
+        std::cout << "Продолжить выполнение программы? (y/n): ";
+        char answer;
+        std::cin >> answer;
+
+        switch (answer)
+        {
+        case('Y'):
+        case('y'):
+
+            endOfProg = false;
+            break;
+
+        case('N'):
+        case('n'):
+
+            endOfProg = true;
+            break;
+
+        default:
+
+            std::cout << "Сочтем это за 'нет'";
+            endOfProg = true;
+        }
+    } while (!endOfProg);
+}
---
+++ Ex_2/Ex_2.cpp
@@ -0,0 +1,91 @@
+﻿#include <iostream>
+
+
+class Counter
+{
+private:
+
+    int value = 0;
+
+public:
+
+    Counter()
+    {
+        std::cout << "Вы желаете ввести начальное значение счетчика? (y/n)" << std::endl;
+        char answer;
+        std::cin >> answer;
+
+        switch (answer)
+        {
+        case('Y'):
+        case('y'):
+            std::cout << "Введите значение, которое хотите установить: ";
+            std::cin >> value;
+            std::cout << std::endl;
+            break;
+
+        case('N'):
+        case('n'):
+            std::cout << "Будет использовано значение по умолчанию(1)\n" << std::endl;
+            value = 1;
+            break;
+
+        default:
+            std::cout << "Так как ответ не был распознан, будет использовано значение по умолчанию(1)\n" << std::endl;
+            value = 1;
+        }
+    }
+
+    void add()
+    {
+        ++value;
+    }
+
+    void subtract()
+    {
+        --value;
+    }
+
+    int getValue()
+    {
+        return value;
+    }
+};
+
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    system("chcp 1251");
+
+    
+    Counter counter;
+    char answer;
+    do
+    {
+        std::cout << "Введите команду (+, -, = или x): ";
+        std::cin >> answer;
+
+        switch (answer)
+        {
+        case('+'):
+            counter.add();
+            break;
+
+        case('-'):
+            counter.subtract();
+            break;
+
+        case('='):
+            std::cout << counter.getValue() << std::endl;
+            break;
+
+        case('x'):
+            std::cout << "Завершение работы программы!" << std::endl;
+            break;
+
+        default:
+            std::cout << "Введена некорректная команда!";
+        }
+    } while (answer != 'x');
+}
